0ad77147ba4b88662047a38dec1271102db11785
Nguyen Quang Minh <nguyenquangminh081003@gmail.com>
2026-02-12 10:06:52 +0700

    fix(add_prescription_link_to_invoice): fix bug migrate and InventoryReportRepository

--- src/main/java/com/project/base_v1/repository/InventoryReportRepository.java
+++ src/main/java/com/project/base_v1/repository/InventoryReportRepository.java
@@ -1,14 +1,14 @@
 package com.project.base_v1.repository;
 
 import com.project.base_v1.repository.projection.MedicineStockSummaryProjection;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.Repository;
 
 import java.time.LocalDate;
 import java.util.List;
 import java.util.UUID;
 
-public interface InventoryReportRepository extends Repository<Object, UUID> {
+public interface InventoryReportRepository extends JpaRepository<com.project.base_v1.entity.Medicine, UUID> {
 
     @Query(value = """
             SELECT
